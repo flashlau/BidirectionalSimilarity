71f81353eddda9b0d39271927607ee9676e11618
Rosaniline <xup6qup3@gmail.com>
2013-04-28 00:28:16 +0800

    Initial Commit

---
+++ BidirectionalSimilarity/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BidirectionalSimilarity
+//
+//  Created by Rosani Lin on 13/4/28.
+//  Copyright (c) 2013年 Rosani Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
